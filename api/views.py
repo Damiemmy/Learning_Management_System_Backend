d2120c5b2527700f25fa802e862a00cc69736512
damiemmy <damisaemmanuel778@gmail.com>
2025-10-05 23:04:53 -0700

    Features:Random OTP for Email Verification/Passord Reset Email,Password Changed Api View

--- api/views.py
+++ api/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from api import serializers as api_serializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import AllowAny
-from rest_framework import generics
+from rest_framework import generics,status
 from userauth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+import random
+from rest_framework.response import Response
+
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -11,6 +15,50 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 class RegisterView(generics.CreateAPIView):
     queryset=User.objects.all()
-    permission_class=[AllowAny]
+    permission_classes=[AllowAny]
     serializer_class=api_serializer.RegisterSerializer
-# Create your views here.
+def generate_random_otp(length=7):
+    otp=''.join([str(random.randint(0,9)) for _ in range(length)])
+    return otp
+class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
+    permission_classes= [AllowAny]
+    serializer_class=api_serializer.UserSerializer
+
+    def get_object(self):
+        email=self.kwargs['email']
+
+        user=User.objects.filter(email=email).first()
+
+        if user:
+
+            uuidb64=user.pk
+            refresh=RefreshToken.for_user(user)
+            refresh_token=str(refresh.access_token)
+            user.refresh_token=refresh_token
+            user.otp=generate_random_otp()
+            user.save()
+
+            link= f"http://localhost:513/create-new-password/?otp{user.otp}&uuidb64={uuidb64}&=refresh_token{refresh_token}"
+            print("link ======",link)
+        
+        return user
+class PasswordChangeAPIView(generics.CreateAPIView):
+    permission_classes=[AllowAny]
+    serializer_class=api_serializer.UserSerializer
+
+    def create(self,request,*args,**kwargs):
+        payload=request.data
+
+        otp=payload['otp']
+        uuidb64=payload['uuid64']
+        password=payload['password']
+
+        user=User.objects.get(id=uuidb64,otp=otp)
+        if user:
+            user.set_password(password)
+            user.otp=""
+            user.save()
+
+            return Response({"message":"Password Changed Successfully"},status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message":"User Does Not Exists"},status=status.HTTP_404_NOT_FOUND)
